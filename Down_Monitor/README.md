b44ec5463d29885f7ba6d5510b4ffe3d7d2405ca
5a5a8 <5a5a8@protonmail.com>
2020-10-05 21:14:04 +1300

    added email notification functionality

--- Down_Monitor/README.md
+++ Down_Monitor/README.md
@@ -3,4 +3,6 @@ A program written to monitor the status of websites.
 Any response status other than OK is considered 'down'.
 
 Domains can be added in the domains.txt file. 
-Domains will be checked every 30 seconds, this interval can be changed at the top of the script.
+
+The settings can be found near the top of the downmonitor.py
+When configured correctly it will notify a specified email address when a domain goes down for more than the specified amount of time.
--- Down_Monitor/downmonitor.py
+++ Down_Monitor/downmonitor.py
@@ -1,9 +1,16 @@
 import requests
 import time
 from datetime import datetime
+import smtplib
 
+###SETTINGS###
 domains_file = 'domains.txt' 
-time_interval = 30 
+time_interval = 30							#check status every x seconds 
+notify_at = 10								#email the user after the site has been down for x time intervals
+gmail_username = 'example@gmail.com' 		#'allow less secure apps' should be set to yes in gmail settings
+gmail_password = 'password123' 				#recommend using a dedicated account for security reasons
+email_recipient = 'example@example.com' 	#who to notify if a site is down
+
 
 #open the domains file and return data as a list
 def get_domains(domains_file = 'domains.txt'):
@@ -18,11 +25,14 @@ def test_domain(domain):
 	try:
 		response = requests.get(domain)
 		if response.status_code == requests.codes.ok:
+			domains_down_time[domain] = 0
 			return True
 		else:
+			domains_down_time[domain] += 1
 			domain_down(domain)
 			return False
 	except: 
+		domains_down_time[domain] += 1
 		domain_down(domain)
 		return False
 	
@@ -35,9 +45,35 @@ def domain_down(domain):
 	down_log.write(domain + ' was down at UTC: ' + datetime.utcnow().strftime('%Y %b %d %H:%M:%S') + '\n')
 	down_log.close()
 
+	#if the domain has been down equal to specified number of allowed intervals, send an email to the user
+	#we don't use > otherwise it will send more than one email
+	if domains_down_time[domain] == notify_at:
+		notify_user(domain)
 
 
+def notify_user(domain):
+	#start the smtp session
+	#need to add better error catching here specific to smtplib
+	print('[INFO] Notifying recipient of outage at ' + domain)
+	try:
+		smtp_notify = smtplib.SMTP('smtp.gmail.com', 587) 
+		smtp_notify.ehlo()
+		smtp_notify.starttls()
+		smtp_notify.login(gmail_username, gmail_password)
+		smtp_notify.sendmail(gmail_username, email_recipient, 'Subject: ' + domain + ' is down \n ')
+		smtp_notify.quit()
+	except:
+		print('[FAIL] Failed to send email to recipient')
+		return
+	else:
+		print(email_recipient + ' has been notified of the outage at ' + domain)
+
+#list of domains
 domains = get_domains(domains_file)
+
+#dictionary of domains with down time
+domains_down_time = {domain: 0 for domain in domains}
+
 while True:
 	#test each domain
 	for domain in domains:
